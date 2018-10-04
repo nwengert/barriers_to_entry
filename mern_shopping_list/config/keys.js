4c4a61b4abdcdf8e2d405775fabf35eee40ebaa3
Nathan Wengert <nate.weng@gmail.com>
2018-10-03 22:57:41 -0600

    first commit

---
+++ mern_shopping_list/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://nate:nate123@ds149412.mlab.com:49412/nw_mern_shopping'
+}
---
+++ mern_shopping_list/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+// bodyParser MiddleWare
+app.use(bodyParser.json());
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//Connect to Mongo
+mongoose
+  .connect(db)
+  .then(()=> console.log('MongoDB Connected.... stand up and shout'))
+  .catch(err => console.log(err));
+
+//Use Routes
+app.use('/api/items', items);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started running briskly on port ${port}`))
