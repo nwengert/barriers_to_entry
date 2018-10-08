75bc5eae2e6ba7a6ab9dd8e75b534a440eead485
Nathan Wengert <nate.weng@gmail.com>
2018-10-08 15:19:06 -0600

    fixed mongoose bug, deploying

--- mern_shopping_list/server.js
+++ mern_shopping_list/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -12,21 +13,36 @@ app.use(bodyParser.json());
 //DB config
 const db = require('./config/keys').mongoURI;
 
+
+//-nw deal with deprication warnings
+//this was from Bob in the user authentification lessons.  Helped to make this app work!
+mongoose.connect("mongodb://localhost/todo-auth-example",
+    {useMongoClient: true},  // helps get rid of deprecation warnings
+    (err) => {
+        if (err) throw err;
+        console.log("Connected to the database");
+    }
+);
+
 //Connect to Mongo
 mongoose
   .connect(db)
   .then(()=> console.log('MongoDB Connected.... stand up and shout'))
   .catch(err => console.log(err));
 
-//Deal with the DepricationWarning  -nw found on google
-mongoose.connect('url', { useNewUrlParser: true }, (err, res) => {
-  if (err) throw err;
-  console.log('Database online thanks to googlers');
-  });
-
   
 //Use Routes
 app.use('/api/items', items);
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //set a static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
