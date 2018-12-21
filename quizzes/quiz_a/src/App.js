e2e1641cdcfc74b35ce038c88987f3a020822446
Nathan Wengert <nate.weng@gmail.com>
2018-12-20 22:25:49 -0700

    starting quiz apps

---
+++ quizzes/quiz_a/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick(){
+        
+    }
+}
